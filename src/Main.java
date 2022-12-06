068fbd4d83e442f58aaace8a2623bef7a6e9fbcd
Seydalievnikolay <nikolay-seydalie@yandex.ru>
2022-12-06 07:31:21 +0700

    conditional 4 task

--- src/Main.java
+++ src/Main.java
@@ -6,27 +6,28 @@ public class Main {
         task1();
         task2();
         task3();
+        task4();
     }
 
     public static void task1(){
         System.out.println("Задача1");
-        int human = 18;
-        if (human >= 18); {
+        int humanAge = 18;
+        if (humanAge >= 18) {
             System.out.println("Если возраст человека равен 18, то он совершеннолетний");
         }
-        if (human < 18); {
+        if (humanAge < 18) {
             System.out.println("Если возраст человека меньше 18,то он не достиг совершеннолетия, нужно немного подождать».");
         }
     }
     public static void task2(){
         System.out.println("Задача2");
         int airTemperatureLower = 5;
-        if (airTemperatureLower < 5);{
+        if (airTemperatureLower < 5) {
             System.out.println("На улице ниже 5 градусов, нужно надеть шапку");
+        } else {
+                System.out.println("На улице выше 5 градусов, можно идти без  шапки");
         }
-        if (airTemperatureLower < 5);{
-            System.out.println("На улице выше 5 градусов, можно идти без  шапки");
-        }
+
     }
 
 
@@ -35,12 +36,15 @@ public class Main {
     public static void task3() {
         System.out.println("Задача3");
         int driverSpeed = 60;
-        if (driverSpeed > 60); {
+        if (driverSpeed > 60) {
             System.out.println("Если скорость больше " + driverSpeed + " ,то придется платить штраф");
         }
 
-        if (driverSpeed < 60); {
+        if (driverSpeed < 60) {
             System.out.println("Если скорость менише " + driverSpeed + " ,можно ездить спокойно");
         }
+    }
+    public static void task4() {
+        System.out.println("Задача4");
     }
 }
